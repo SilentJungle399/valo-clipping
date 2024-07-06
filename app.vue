5569e0a4549238739fc9b2f20a9e18250b9a0750
SilentJungle399 <vardhan.harsh4041@gmail.com>
2024-07-06 21:43:43 +0530

    initial commit

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="">
+		<input type="file" accept="video/*" @change="checkUpload" />
+
+		<video controls ref="videoPlayer"></video>
+	</div>
+</template>
+
+<script setup>
+const videoPlayer = ref(null);
+
+const checkUpload = (e) => {
+	const file = e.target.files[0];
+	if (!file) return;
+
+	const url = URL.createObjectURL(file);
+	videoPlayer.value.src = url;
+	videoPlayer.value.play();
+};
+</script>
