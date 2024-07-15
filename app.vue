5820f6b918d1ee495e26aa5652a790d82cf6f58d
SilentJungle399 <vardhan.harsh4041@gmail.com>
2024-07-15 16:03:08 +0530

    no socketio either

--- app.vue
+++ app.vue
@@ -3,15 +3,13 @@
 		<input type="file" accept="video/*" @change="checkUpload" />
 		<button @click="cleanPlayer">Close</button>
 
-		<video controls ref="videoPlayer" v-show="showPlayer" width="300"></video>
+		<video ref="videoPlayer" v-show="showPlayer" width="300"></video>
 	</div>
 </template>
 
 <script setup>
 const videoPlayer = ref(null);
 const showPlayer = ref(false);
-
-const { $io } = useNuxtApp();
 
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -22,32 +20,6 @@ const checkUpload = (e) => {
 	const url = URL.createObjectURL(file);
 	videoPlayer.value.src = url;
 	showPlayer.value = true;
-
-	const videoFile = e.target.files[0];
-
-	const chunkSize = 0.5 * 1024 * 1024; // 1MB chunks (adjust as needed)
-	let chunkStart = 0;
-	let totalChunks = Math.ceil(videoFile.size / chunkSize);
-
-	const reader = new FileReader();
-	reader.onload = async (e) => {
-		const chunk = e.target.result;
-		$io.emit("video_chunk", {
-			name: videoFile.name,
-			data: chunk,
-			chunkNumber: chunkStart / chunkSize + 1,
-			totalChunks,
-		});
-		chunkStart += chunkSize;
-
-		if (chunkStart < videoFile.size) {
-			reader.readAsArrayBuffer(videoFile.slice(chunkStart, chunkStart + chunkSize));
-		} else {
-			$io.emit("video_end", { name: videoFile.name }); // Signal end of transmission
-		}
-	};
-
-	reader.readAsArrayBuffer(videoFile.slice(0, chunkSize));
 };
 
 const cleanPlayer = () => {
