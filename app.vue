463576323bc470ca8eb0bba5abac40baf838b015
SilentJungle399 <vardhan.harsh4041@gmail.com>
2024-07-15 20:09:10 +0530

    added UI for browsing directories

--- app.vue
+++ app.vue
@@ -1,23 +1,37 @@
 <template>
 	<div class="container">
+		<Video
+			v-for="item in files"
+			:key="item.name"
+			:name="item.name"
+			:duration="item.duration"
+			:thumb="item.thumb"
+		></Video>
 	</div>
 </template>
 
 <script setup>
-onMounted(async () => {
-	const files = await window.ipc.getFiles();
-	console.log(files);
-});
+const files = ref(await window.ipc.getFiles());
+console.log(files);
+// onMounted(async () => {
+// 	;
+// 	console.log(files);
+// });
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+
 body {
-	background: #000;
+	font-family: "Poppins", sans-serif;
+	background: #f6f6f6;
+	/* color: #000; */
 }
 
 .container {
 	display: flex;
-	flex-direction: column;
-	align-items: center;
+	flex-direction: row;
+	flex-wrap: wrap;
+	justify-content: center;
 }
 </style>
---
+++ components/Video.vue
@@ -0,0 +1,85 @@
+<template>
+	<div class="video">
+		<div class="thumbnail">
+			<img :src="`data:image/jpeg;base64,${props.thumb}`" v-if="thumb" alt="thumb" />
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" v-else>
+				<path
+					d="M64 480H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H288c-10.1 0-19.6-4.7-25.6-12.8L243.2 57.6C231.1 41.5 212.1 32 192 32H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64z"
+				/>
+			</svg>
+			<span v-if="thumb" class="duration">
+				{{ Math.floor(duration / 60) }}:{{ String(duration % 60).padStart(2, "0") }}
+			</span>
+		</div>
+		<span class="name" :title="name">{{
+			name.length > 30 ? name.slice(0, 30) + "..." : name
+		}}</span>
+	</div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+	name: string;
+	duration: number;
+	thumb: string;
+}>();
+</script>
+
+<style scoped>
+.video {
+	display: flex;
+	flex-direction: column;
+	margin: 10px 25px;
+	background-color: #f0f0f0;
+	width: 400px;
+	height: 280px;
+	border-radius: 10px;
+	transition: all 0.3s;
+}
+
+.video:hover {
+	cursor: pointer;
+	box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+}
+
+.thumbnail {
+	display: flex;
+	flex-direction: column;
+	background-color: #fff;
+	justify-content: center;
+	position: relative;
+	border-radius: 10px;
+	height: 100%;
+}
+
+.duration {
+	position: absolute;
+	bottom: 0;
+	right: 0;
+	background: rgba(0, 0, 0, 0.5);
+	color: #fff;
+	padding: 0px 5px;
+	margin: 5px 7px;
+	border-radius: 5px;
+}
+
+.name {
+	font-weight: 500;
+	margin: 15px 30px;
+}
+
+img {
+	width: 400px;
+	height: auto;
+	border-radius: 6px;
+}
+
+svg {
+	fill: #583da1;
+	width: 190px;
+	position: absolute;
+	top: 15px;
+	left: 30px;
+	filter: drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.5));
+}
+</style>
