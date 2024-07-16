904b6fc1c64cfa2874c9ca8fdaa2fdab0deb07a8
SilentJungle399 <vardhan.harsh4041@gmail.com>
2024-07-16 22:46:21 +0530

    created a seekbar for clipping

---
+++ components/SeekBar.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="seekbar">
+		<div class="progress"></div>
+	</div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+	metadata: object;
+}>();
+
+onMounted(() => {
+	const video = document.getElementById("video") as HTMLVideoElement;
+
+	video.addEventListener("timeupdate", () => {
+		const progress = (video.currentTime / video.duration) * 100;
+		const progressBar = document.querySelector(".progress") as HTMLElement;
+		progressBar.style.background = `linear-gradient(to right, #d0d0d0 ${progress}%, #e9e9e9 ${progress}%)`;
+	});
+});
+</script>
+
+<style scoped>
+.seekbar {
+	width: 95vw;
+	height: 40px;
+	margin-top: 20px;
+}
+
+.progress {
+	height: 100%;
+	width: 100%;
+	background: linear-gradient(to right, #d0d0d0 0%, #f0f0f0 0%);
+	border-radius: 10px;
+	transition: all 0.1s;
+}
+</style>
